6e0e01d39bd3ba520d07ad9e663d03b31f6ad203
Huang Yang <elious.huang@gmail.com>
2019-07-27 16:33:37 +0800

    temp

--- mesh/meshx_config.h
+++ mesh/meshx_config.h
@@ -17,6 +17,8 @@
 #define MESHX_TRACE_FAST                           0
 #define MESHX_ENABLE_ASSERT                        1
 
+#define MESHX_USE_SAMPLE_DATA                      1
+
 /* gap adv */
 #define MESHX_GAP_ACTION_MAX_NUM                   10
 
