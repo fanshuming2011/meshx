65319885b8aee084a9c346492480ae4fb9a90b90
Huang Yang <elious.huang@gmail.com>
2019-10-20 00:07:09 +0800

    temp

--- mesh/transport/meshx_upper_transport.c
+++ mesh/transport/meshx_upper_transport.c
@@ -14,6 +14,7 @@
 #include "meshx_key.h"
 #include "meshx_security.h"
 #include "meshx_endianness.h"
+#include "meshx_node_internal.h"
 
 #define MESHX_UNSEG_ACCESS_MAX_PDU_SIZE                    15
 #define MESHX_UPPER_TRANPORT_MAX_CTL_PDU_SIZE              256
@@ -26,22 +27,22 @@ int32_t meshx_upper_transport_init(void)
 
 static void meshx_upper_transport_encrypt(uint8_t *paccess_pdu, uint8_t pdu_len,
                                           uint8_t *ptrans_mic, uint8_t trans_mic_len,
-                                          const meshx_msg_ctx_t *pmsg_ctx)
+                                          const meshx_msg_ctx_t *pmsg_tx_ctx)
 {
     uint8_t nonce[MESHX_NONCE_SIZE];
-    if (pmsg_ctx->akf)
+    if (pmsg_tx_ctx->akf)
     {
         meshx_application_nonce_t *papp_nonce = (meshx_application_nonce_t *)nonce;
         papp_nonce->nonce_type = MESHX_NONCE_TYPE_APPLICAITON;
         papp_nonce->pad = 0;
         /* TODO: only valid in segment access message, other message shall be 0 */
-        papp_nonce->aszmic = pmsg_ctx->szmic;
-        papp_nonce->seq[0] = pmsg_ctx->seq_origin >> 16;
-        papp_nonce->seq[1] = pmsg_ctx->seq_origin >> 8;
-        papp_nonce->seq[2] = pmsg_ctx->seq_origin;
-        papp_nonce->src = MESHX_HOST_TO_BE16(pmsg_ctx->src);
-        papp_nonce->dst = MESHX_HOST_TO_BE16(pmsg_ctx->dst);
-        papp_nonce->iv_index = MESHX_HOST_TO_BE32(pmsg_ctx->iv_index);
+        papp_nonce->aszmic = pmsg_tx_ctx->szmic;
+        papp_nonce->seq[0] = pmsg_tx_ctx->seq_origin >> 16;
+        papp_nonce->seq[1] = pmsg_tx_ctx->seq_origin >> 8;
+        papp_nonce->seq[2] = pmsg_tx_ctx->seq_origin;
+        papp_nonce->src = MESHX_HOST_TO_BE16(pmsg_tx_ctx->src);
+        papp_nonce->dst = MESHX_HOST_TO_BE16(pmsg_tx_ctx->dst);
+        papp_nonce->iv_index = MESHX_HOST_TO_BE32(pmsg_tx_ctx->iv_index);
         MESHX_DEBUG("application nonce:");
         MESHX_DUMP_DEBUG(papp_nonce, sizeof(meshx_application_nonce_t));
     }
@@ -51,13 +52,13 @@ static void meshx_upper_transport_encrypt(uint8_t *paccess_pdu, uint8_t pdu_len,
         pdev_nonce->nonce_type = MESHX_NONCE_TYPE_DEVICE;
         pdev_nonce->pad = 0;
         /* TODO: only valid in segment access message, other message shall be 0 */
-        pdev_nonce->aszmic = pmsg_ctx->szmic;
-        pdev_nonce->seq[0] = pmsg_ctx->seq_origin >> 16;
-        pdev_nonce->seq[1] = pmsg_ctx->seq_origin >> 8;
-        pdev_nonce->seq[2] = pmsg_ctx->seq_origin;
-        pdev_nonce->src = MESHX_HOST_TO_BE16(pmsg_ctx->src);
-        pdev_nonce->dst = MESHX_HOST_TO_BE16(pmsg_ctx->dst);
-        pdev_nonce->iv_index = MESHX_HOST_TO_BE32(pmsg_ctx->iv_index);
+        pdev_nonce->aszmic = pmsg_tx_ctx->szmic;
+        pdev_nonce->seq[0] = pmsg_tx_ctx->seq_origin >> 16;
+        pdev_nonce->seq[1] = pmsg_tx_ctx->seq_origin >> 8;
+        pdev_nonce->seq[2] = pmsg_tx_ctx->seq_origin;
+        pdev_nonce->src = MESHX_HOST_TO_BE16(pmsg_tx_ctx->src);
+        pdev_nonce->dst = MESHX_HOST_TO_BE16(pmsg_tx_ctx->dst);
+        pdev_nonce->iv_index = MESHX_HOST_TO_BE32(pmsg_tx_ctx->iv_index);
         MESHX_DEBUG("device nonce:");
         MESHX_DUMP_DEBUG(pdev_nonce, sizeof(meshx_device_nonce_t));
     }
@@ -66,8 +67,8 @@ static void meshx_upper_transport_encrypt(uint8_t *paccess_pdu, uint8_t pdu_len,
     uint8_t *padd = NULL;
     uint8_t add_len = 0;
 
-    const meshx_key_t *pencrypt_key = pmsg_ctx->akf ? pmsg_ctx->papp_key :
-                                      pmsg_ctx->pdev_key;
+    const meshx_key_t *pencrypt_key = pmsg_tx_ctx->akf ? pmsg_tx_ctx->papp_key :
+                                      pmsg_tx_ctx->pdev_key;
     /* encrypt data */
     meshx_aes_ccm_encrypt(*pencrypt_key, nonce, MESHX_NONCE_SIZE,
                           padd, add_len, paccess_pdu, pdu_len, paccess_pdu, ptrans_mic, trans_mic_len);
@@ -75,6 +76,97 @@ static void meshx_upper_transport_encrypt(uint8_t *paccess_pdu, uint8_t pdu_len,
     MESHX_DUMP_DEBUG(paccess_pdu, pdu_len);
     MESHX_DEBUG("access pdu transmic:");
     MESHX_DUMP_DEBUG(ptrans_mic, trans_mic_len);
+}
+
+static int32_t meshx_upper_transport_decrypt(uint8_t *paccess_pdu, uint8_t pdu_len,
+                                             uint8_t *ptrans_mic, uint8_t trans_mic_len,
+                                             meshx_msg_ctx_t *pmsg_rx_ctx)
+{
+    int32_t ret = MESHX_SUCCESS;
+    uint8_t nonce[MESHX_NONCE_SIZE];
+    if (pmsg_rx_ctx->akf)
+    {
+        meshx_application_nonce_t *papp_nonce = (meshx_application_nonce_t *)nonce;
+        papp_nonce->nonce_type = MESHX_NONCE_TYPE_APPLICAITON;
+        papp_nonce->pad = 0;
+        /* TODO: only valid in segment access message, other message shall be 0 */
+        papp_nonce->aszmic = pmsg_rx_ctx->szmic;
+        papp_nonce->seq[0] = pmsg_rx_ctx->seq_origin >> 16;
+        papp_nonce->seq[1] = pmsg_rx_ctx->seq_origin >> 8;
+        papp_nonce->seq[2] = pmsg_rx_ctx->seq_origin;
+        papp_nonce->src = MESHX_HOST_TO_BE16(pmsg_rx_ctx->src);
+        papp_nonce->dst = MESHX_HOST_TO_BE16(pmsg_rx_ctx->dst);
+        papp_nonce->iv_index = MESHX_HOST_TO_BE32(pmsg_rx_ctx->iv_index);
+        MESHX_DEBUG("application nonce:");
+        MESHX_DUMP_DEBUG(papp_nonce, sizeof(meshx_application_nonce_t));
+
+        /* get app key */
+        const meshx_application_key_t *papp_key = NULL;
+        meshx_app_key_traverse_start(&papp_key, pmsg_rx_ctx->aid);
+        if (NULL == papp_key)
+        {
+            MESHX_INFO("no key's aid is 0x%x", pmsg_rx_ctx->aid);
+            return -MESHX_ERR_KEY;
+        }
+
+        int32_t ret = MESHX_SUCCESS;
+        /* TODO: label uuid */
+        uint8_t *padd = NULL;
+        uint8_t add_len = 0;
+        while (NULL != papp_key)
+        {
+            ret = meshx_aes_ccm_decrypt(papp_key->app_key, nonce, MESHX_NONCE_SIZE,
+                                        padd, add_len, paccess_pdu, pdu_len, paccess_pdu, ptrans_mic, trans_mic_len);
+            if (MESHX_SUCCESS == ret)
+            {
+                break;
+            }
+            else
+            {
+                meshx_app_key_traverse_continue(&papp_key, pmsg_rx_ctx->aid);
+            }
+        }
+
+        if (NULL == papp_key)
+        {
+            MESHX_WARN("can't decrypt pdu by application key that aid is 0x%x", pmsg_rx_ctx->aid);
+            return -MESHX_ERR_KEY;
+        }
+        MESHX_DEBUG("decrypt access pdu:");
+        MESHX_DUMP_DEBUG(paccess_pdu, pdu_len);
+    }
+    else
+    {
+        meshx_device_nonce_t *pdev_nonce = (meshx_device_nonce_t *)nonce;
+        pdev_nonce->nonce_type = MESHX_NONCE_TYPE_DEVICE;
+        pdev_nonce->pad = 0;
+        /* TODO: only valid in segment access message, other message shall be 0 */
+        pdev_nonce->aszmic = pmsg_rx_ctx->szmic;
+        pdev_nonce->seq[0] = pmsg_rx_ctx->seq_origin >> 16;
+        pdev_nonce->seq[1] = pmsg_rx_ctx->seq_origin >> 8;
+        pdev_nonce->seq[2] = pmsg_rx_ctx->seq_origin;
+        pdev_nonce->src = MESHX_HOST_TO_BE16(pmsg_rx_ctx->src);
+        pdev_nonce->dst = MESHX_HOST_TO_BE16(pmsg_rx_ctx->dst);
+        pdev_nonce->iv_index = MESHX_HOST_TO_BE32(pmsg_rx_ctx->iv_index);
+        MESHX_DEBUG("device nonce:");
+        MESHX_DUMP_DEBUG(pdev_nonce, sizeof(meshx_device_nonce_t));
+
+        /* TODO: label uuid */
+        uint8_t *padd = NULL;
+        uint8_t add_len = 0;
+
+        /* decrypt data */
+        pmsg_rx_ctx->pdev_key = &meshx_dev_key_get(meshx_node_params.param.node_addr)->dev_key;
+        ret = meshx_aes_ccm_decrypt(*pmsg_rx_ctx->pdev_key, nonce, MESHX_NONCE_SIZE,
+                                    padd, add_len, paccess_pdu, pdu_len, paccess_pdu, ptrans_mic, trans_mic_len);
+        if (MESHX_SUCCESS == ret)
+        {
+            MESHX_DEBUG("decrypt access pdu:");
+            MESHX_DUMP_DEBUG(paccess_pdu, pdu_len);
+        }
+    }
+
+    return ret;
 }
 
 int32_t meshx_upper_transport_send(meshx_network_if_t network_if,
@@ -116,7 +208,7 @@ int32_t meshx_upper_transport_send(meshx_network_if_t network_if,
         uint8_t trans_mic_len = 4;
         if (pmsg_tx_ctx->szmic)
         {
-            trans_mic_len += 4;
+            trans_mic_len = 8;
         }
 
         /* TODO: use allocate data instead of stack data? */
@@ -139,6 +231,7 @@ int32_t meshx_upper_transport_receive(meshx_network_if_t network_if,
     MESHX_DEBUG("receive upper transport pdu: type %d", pmsg_rx_ctx->ctl);
     MESHX_DUMP_DEBUG(pdata, len);
 
+    int32_t ret = MESHX_SUCCESS;
     if (pmsg_rx_ctx->ctl)
     {
         /* receive control message */
@@ -146,8 +239,29 @@ int32_t meshx_upper_transport_receive(meshx_network_if_t network_if,
     else
     {
         /* receive access message */
+        /* decrypt access message */
+        uint8_t trans_mic_len = 4;
+        if (pmsg_rx_ctx->seg && pmsg_rx_ctx->szmic)
+        {
+            trans_mic_len = 8;
+        }
+
+        /* TODO: use allocate data instead of stack data? */
+        uint8_t pdu[MESHX_UPPER_TRANPORT_MAX_ACCESS_PDU_SIZE + 4];
+        memcpy(pdu, pdata, len);
+
+        ret = meshx_upper_transport_decrypt(pdu, len - trans_mic_len, pdu + len - trans_mic_len,
+                                            trans_mic_len, pmsg_rx_ctx);
+        if (MESHX_SUCCESS == ret)
+        {
+            /* notify access layer */
+        }
+        else
+        {
+            MESHX_WARN("decrypt access pdu failed!");
+        }
     }
 
-    return MESHX_SUCCESS;
+    return ret;
 }
 
